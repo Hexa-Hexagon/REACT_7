1d8953063f105d6a8d1e9ed6ccf799078a7014b3
Ehor <ehorlukianov@gmail.com>
2023-09-27 13:23:29 +0300

    add first TODO code in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,42 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  return <h2>TODO лист на React ЛР 7</h2>;
+    const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+    const [isEditing, setIsEditing] = useState(0);
+    const [editID, setEditID] = useState(null);
+    const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return ( <
+        section className = "section-center" >
+        <
+        form className = "grocery-form"
+        onSubmit = { handleSubmit } > { alert.show && < Alert / > } <
+        h3 > TODO List < /h3> <
+        div className = "form-control" >
+        <
+        input type = "text"
+        className = "grocery"
+        placeholder = "e.g. pass the exam"
+        value = { name }
+        onChange = {
+            (e) => setName(e.target.value) }
+        /> <
+        button type = "submit"
+        className = "submit-btn" > { isEditing ? "edit" : "submit" } < /button> <
+        /div> <
+        /form> <
+        div className = "grocery-container" >
+        <
+        List / >
+        <
+        button className = "clear-btn" > clear items < /button> <
+        /div> <
+        /section>
+    );
 }
 
 export default App;
